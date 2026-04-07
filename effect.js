8a525f2a2f2c602fe130cae8ea0eb78dd3cf48d1
aditya <154753473+adityatheQuantumCoder@users.noreply.github.com>
2026-04-07 23:39:53 +0530

    Update balloon animation logic and fix typo
    Refactor balloon animations and fix background color typo

--- effect.js
+++ effect.js
@@ -2,18 +2,22 @@ $(window).load(function(){
 	$('.loading').fadeOut('fast');
 	$('.container').fadeIn('fast');
 });
+
 $('document').ready(function(){
 		var vw;
 		$(window).resize(function(){
-			 vw = $(window).width()/2;
+			vw = $(window).width()/2;
 			$('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
+			// We only resize the generic balloons (1, 2, 7)
 			$('#b11').animate({top:240, left: vw-350},500);
 			$('#b22').animate({top:240, left: vw-250},500);
-			$('#b33').animate({top:240, left: vw-150},500);
-			$('#b44').animate({top:240, left: vw-50},500);
-			$('#b55').animate({top:240, left: vw+50},500);
-			$('#b66').animate({top:240, left: vw+150},500);
 			$('#b77').animate({top:240, left: vw+250},500);
+			
+			// Let CSS handle the centering for ARYA (3, 4, 5, 6)
+			$('#b33').animate({top:240},500); 
+			$('#b44').animate({top:240},500);
+			$('#b55').animate({top:240},500);
+			$('#b66').animate({top:240},500);
 		});
 
 	$('#turn_on').click(function(){
@@ -28,6 +32,7 @@ $('document').ready(function(){
 			$('#play').fadeIn('slow');
 		});
 	});
+
 	$('#play').click(function(){
 		var audio = $('.song')[0];
         audio.play();
@@ -37,7 +42,7 @@ $('document').ready(function(){
 		$('#bulb_green').addClass('bulb-glow-green-after');
 		$('#bulb_pink').addClass('bulb-glow-pink-after');
 		$('#bulb_orange').addClass('bulb-glow-orange-after');
-		$('body').css('backgroud-color','#FFF');
+		$('body').css('background-color','#FFF'); // Fixed typo from 'backgroud-color'
 		$('body').addClass('peach-after');
 		$(this).fadeOut('slow').delay(6000).promise().done(function(){
 			$('#bannar_coming').fadeIn('slow');
@@ -51,66 +56,49 @@ $('document').ready(function(){
 		});
 	});
 
+	/* --- BALLOON ANIMATION LOOPS --- */
+	
+	// Balloons 1, 2, and 7 remain totally random
 	function loopOne() {
 		var randleft = 1000*Math.random();
 		var randtop = 500*Math.random();
-		$('#b1').animate({left:randleft,bottom:randtop},10000,function(){
-			loopOne();
-		});
+		$('#b1').animate({left:randleft,bottom:randtop},10000,function(){ loopOne(); });
 	}
 	function loopTwo() {
 		var randleft = 1000*Math.random();
 		var randtop = 500*Math.random();
-		$('#b2').animate({left:randleft,bottom:randtop},10000,function(){
-			loopTwo();
-		});
-	}
-	function loopThree() {
-		var randleft = 1000*Math.random();
-		var randtop = 500*Math.random();
-		$('#b3').animate({left:randleft,bottom:randtop},10000,function(){
-			loopThree();
-		});
-	}
-	function loopFour() {
-		var randleft = 1000*Math.random();
-		var randtop = 500*Math.random();
-		$('#b4').animate({left:randleft,bottom:randtop},10000,function(){
-			loopFour();
-		});
-	}
-	function loopFive() {
-		var randleft = 1000*Math.random();
-		var randtop = 500*Math.random();
-		$('#b5').animate({left:randleft,bottom:randtop},10000,function(){
-			loopFive();
-		});
-	}
-
-	function loopSix() {
-		var randleft = 1000*Math.random();
-		var randtop = 500*Math.random();
-		$('#b6').animate({left:randleft,bottom:randtop},10000,function(){
-			loopSix();
-		});
+		$('#b2').animate({left:randleft,bottom:randtop},10000,function(){ loopTwo(); });
 	}
 	function loopSeven() {
 		var randleft = 1000*Math.random();
 		var randtop = 500*Math.random();
-		$('#b7').animate({left:randleft,bottom:randtop},10000,function(){
-			loopSeven();
-		});
+		$('#b7').animate({left:randleft,bottom:randtop},10000,function(){ loopSeven(); });
 	}
+
+	// ARYA Balloons (3, 4, 5, 6) only move vertically. CSS handles their left position.
+	function loopThree() {
+		var randtop = 500*Math.random();
+		$('#b3').animate({bottom:randtop},10000,function(){ loopThree(); });
+	}
+	function loopFour() {
+		var randtop = 500*Math.random();
+		$('#b4').animate({bottom:randtop},10000,function(){ loopFour(); });
+	}
+	function loopFive() {
+		var randtop = 500*Math.random();
+		$('#b5').animate({bottom:randtop},10000,function(){ loopFive(); });
+	}
+	function loopSix() {
+		var randtop = 500*Math.random();
+		$('#b6').animate({bottom:randtop},10000,function(){ loopSix(); });
+	}
+
 
 	$('#balloons_flying').click(function(){
 		$('.balloon-border').animate({top:-500},8000);
 		$('#b1,#b4,#b5,#b7').addClass('balloons-rotate-behaviour-one');
 		$('#b2,#b3,#b6').addClass('balloons-rotate-behaviour-two');
-		// $('#b3').addClass('balloons-rotate-behaviour-two');
-		// $('#b4').addClass('balloons-rotate-behaviour-one');
-		// $('#b5').addClass('balloons-rotate-behaviour-one');
-		// $('#b6').addClass('balloons-rotate-behaviour-two');
-		// $('#b7').addClass('balloons-rotate-behaviour-one');
+		
 		loopOne();
 		loopTwo();
 		loopThree();
@@ -124,8 +112,9 @@ $('document').ready(function(){
 		});
 	});	
 
+	// Note: We changed .cake to .cake-cover here to match your updated HTML/CSS structure
 	$('#cake_fadein').click(function(){
-		$('.cake').fadeIn('slow');
+		$('.cake-cover').fadeIn('slow');
 		$(this).fadeOut('slow').delay(3000).promise().done(function(){
 			$('#light_candle').fadeIn('slow');
 		});
@@ -140,23 +129,28 @@ $('document').ready(function(){
 
 		
 	$('#wish_message').click(function(){
-		 vw = $(window).width()/2;
+		vw = $(window).width()/2;
 
 		$('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
 		$('#b1').attr('id','b11');
-		$('#b2').attr('id','b22')
-		$('#b3').attr('id','b33')
-		$('#b4').attr('id','b44')
-		$('#b5').attr('id','b55')
-		$('#b6').attr('id','b66')
-		$('#b7').attr('id','b77')
+		$('#b2').attr('id','b22');
+		$('#b3').attr('id','b33');
+		$('#b4').attr('id','b44');
+		$('#b5').attr('id','b55');
+		$('#b6').attr('id','b66');
+		$('#b7').attr('id','b77');
+		
+		// Random balloons go to the edges
 		$('#b11').animate({top:240, left: vw-350},500);
 		$('#b22').animate({top:240, left: vw-250},500);
-		$('#b33').animate({top:240, left: vw-150},500);
-		$('#b44').animate({top:240, left: vw-50},500);
-		$('#b55').animate({top:240, left: vw+50},500);
-		$('#b66').animate({top:240, left: vw+150},500);
 		$('#b77').animate({top:240, left: vw+250},500);
+		
+		// ARYA balloons stay centered horizontally, just move to the top
+		$('#b33').animate({top:240},500);
+		$('#b44').animate({top:240},500);
+		$('#b55').animate({top:240},500);
+		$('#b66').animate({top:240},500);
+
 		$('.balloons').css('opacity','0.9');
 		$('.balloons h2').fadeIn(3000);
 		$(this).fadeOut('slow').delay(3000).promise().done(function(){
@@ -166,7 +160,7 @@ $('document').ready(function(){
 	
 	$('#story').click(function(){
 		$(this).fadeOut('slow');
-		$('.cake').fadeOut('fast').promise().done(function(){
+		$('.cake-cover').fadeOut('fast').promise().done(function(){
 			$('.message').fadeIn('slow');
 		});
 		
@@ -178,24 +172,15 @@ $('document').ready(function(){
 			$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
 			if(i==50){
 				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
-					$('.cake').fadeIn('fast');
+					$('.cake-cover').fadeIn('fast');
 				});
-				
 			}
 			else{
 				msgLoop(i);
 			}			
-
 		});
-			// body...
 		}
 		
 		msgLoop(0);
-		
 	});
 });
-
-
-
-
-//alert('hello');
