d74717b53749c7d31d1320ae934ff1248173ac26
aditya <154753473+adityatheQuantumCoder@users.noreply.github.com>
2026-04-07 23:47:19 +0530

    Refactor balloon animation and event handling

--- effect.js
+++ effect.js
@@ -8,13 +8,13 @@ $('document').ready(function(){
 		$(window).resize(function(){
 			vw = $(window).width()/2;
 			$('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
-			// We only resize the generic balloons (1, 2, 7)
+            // Random balloons resize
 			$('#b11').animate({top:240, left: vw-350},500);
 			$('#b22').animate({top:240, left: vw-250},500);
 			$('#b77').animate({top:240, left: vw+250},500);
-			
-			// Let CSS handle the centering for ARYA (3, 4, 5, 6)
-			$('#b33').animate({top:240},500); 
+            
+            // ARYA balloons resize (vertical only, CSS handles left)
+			$('#b33').animate({top:240},500);
 			$('#b44').animate({top:240},500);
 			$('#b55').animate({top:240},500);
 			$('#b66').animate({top:240},500);
@@ -42,7 +42,7 @@ $('document').ready(function(){
 		$('#bulb_green').addClass('bulb-glow-green-after');
 		$('#bulb_pink').addClass('bulb-glow-pink-after');
 		$('#bulb_orange').addClass('bulb-glow-orange-after');
-		$('body').css('background-color','#FFF'); // Fixed typo from 'backgroud-color'
+		$('body').css('background-color','#FFF'); 
 		$('body').addClass('peach-after');
 		$(this).fadeOut('slow').delay(6000).promise().done(function(){
 			$('#bannar_coming').fadeIn('slow');
@@ -56,9 +56,8 @@ $('document').ready(function(){
 		});
 	});
 
-	/* --- BALLOON ANIMATION LOOPS --- */
-	
-	// Balloons 1, 2, and 7 remain totally random
+    /* --- BALLOON LOOPS --- */
+    // 1, 2, and 7 move randomly anywhere on the screen
 	function loopOne() {
 		var randleft = 1000*Math.random();
 		var randtop = 500*Math.random();
@@ -75,7 +74,7 @@ $('document').ready(function(){
 		$('#b7').animate({left:randleft,bottom:randtop},10000,function(){ loopSeven(); });
 	}
 
-	// ARYA Balloons (3, 4, 5, 6) only move vertically. CSS handles their left position.
+    // ARYA Balloons (3, 4, 5, 6) only float straight up so they stay centered
 	function loopThree() {
 		var randtop = 500*Math.random();
 		$('#b3').animate({bottom:randtop},10000,function(){ loopThree(); });
@@ -93,26 +92,19 @@ $('document').ready(function(){
 		$('#b6').animate({bottom:randtop},10000,function(){ loopSix(); });
 	}
 
-
 	$('#balloons_flying').click(function(){
 		$('.balloon-border').animate({top:-500},8000);
 		$('#b1,#b4,#b5,#b7').addClass('balloons-rotate-behaviour-one');
 		$('#b2,#b3,#b6').addClass('balloons-rotate-behaviour-two');
 		
-		loopOne();
-		loopTwo();
-		loopThree();
-		loopFour();
-		loopFive();
-		loopSix();
-		loopSeven();
+		loopOne(); loopTwo(); loopThree(); loopFour(); loopFive(); loopSix(); loopSeven();
 		
 		$(this).fadeOut('slow').delay(5000).promise().done(function(){
 			$('#cake_fadein').fadeIn('slow');
 		});
 	});	
 
-	// Note: We changed .cake to .cake-cover here to match your updated HTML/CSS structure
+    // Targets .cake-cover for the smooth fade in
 	$('#cake_fadein').click(function(){
 		$('.cake-cover').fadeIn('slow');
 		$(this).fadeOut('slow').delay(3000).promise().done(function(){
@@ -127,30 +119,26 @@ $('document').ready(function(){
 		});
 	});
 
-		
 	$('#wish_message').click(function(){
-		vw = $(window).width()/2;
+		 vw = $(window).width()/2;
 
 		$('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
-		$('#b1').attr('id','b11');
-		$('#b2').attr('id','b22');
-		$('#b3').attr('id','b33');
-		$('#b4').attr('id','b44');
-		$('#b5').attr('id','b55');
-		$('#b6').attr('id','b66');
+		$('#b1').attr('id','b11'); $('#b2').attr('id','b22');
+		$('#b3').attr('id','b33'); $('#b4').attr('id','b44');
+		$('#b5').attr('id','b55'); $('#b6').attr('id','b66');
 		$('#b7').attr('id','b77');
-		
-		// Random balloons go to the edges
+        
+        // Random background balloons to the edges
 		$('#b11').animate({top:240, left: vw-350},500);
 		$('#b22').animate({top:240, left: vw-250},500);
 		$('#b77').animate({top:240, left: vw+250},500);
-		
-		// ARYA balloons stay centered horizontally, just move to the top
+        
+        // ARYA balloons line up neatly (only moving to top: 240)
 		$('#b33').animate({top:240},500);
 		$('#b44').animate({top:240},500);
 		$('#b55').animate({top:240},500);
 		$('#b66').animate({top:240},500);
-
+        
 		$('.balloons').css('opacity','0.9');
 		$('.balloons h2').fadeIn(3000);
 		$(this).fadeOut('slow').delay(3000).promise().done(function(){
@@ -165,7 +153,6 @@ $('document').ready(function(){
 		});
 		
 		var i;
-
 		function msgLoop (i) {
 			$("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
 			i=i+1;
@@ -180,7 +167,6 @@ $('document').ready(function(){
 			}			
 		});
 		}
-		
 		msgLoop(0);
 	});
 });
